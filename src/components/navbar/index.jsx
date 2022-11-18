d27666f22c56fa4b79b8e2df01b0147b2d7da43b
João Vitor Marques Braga <102833436+Joao-Vitor-Marques-Braga@users.noreply.github.com>
2022-11-18 20:03:51 -0300

    Add files via upload

---
+++ src/components/navbar/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style.css'
+
+export default function NavBar(){
+    return(
+        <nav className='nav'>
+            <div className='logo'>
+                <h1>FormsDev</h1>
+            </div>
+        </nav>
+    )
+}
